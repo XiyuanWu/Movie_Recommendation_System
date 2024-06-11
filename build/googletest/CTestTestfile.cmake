386509604a1a0ef0f831eee4f08ce4b18230ba83
Xiyuan Wu <xiyuan.24706826@gmail.com>
2024-06-10 22:43:01 -0700

    Delete build/googletest directory

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/sdash007/final-project-xwu150-czhan251-moseg004-sdash007/googletest
-# Build directory: /home/csmajs/sdash007/final-project-xwu150-czhan251-moseg004-sdash007/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
